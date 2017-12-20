c08998ee24d41bab882d7e864e088c204c8aa959
云行 邓 <271293794@qq.com>
2017-12-20 22:41:48 +0800

    11111

--- DjangoWebProject1/manage.py
+++ DjangoWebProject1/manage.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         "DJANGO_SETTINGS_MODULE",  
         "DjangoWebProject1.settings"
     )
-#!/加注释
+#!/加注释11111
 
     from django.core.management import execute_from_command_line
 
