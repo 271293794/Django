9a91ce5a84e646ad0f94d4ae6fcec657a9e70832
271293794 <30901670+271293794@users.noreply.github.com>
2017-12-21 16:11:57 +0800

    Update manage.py

--- DjangoWebProject1/manage.py
+++ DjangoWebProject1/manage.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         "DJANGO_SETTINGS_MODULE",  
         "DjangoWebProject1.settings"
     )
-#!/加注释111112222233333444455555666677778888
+#!/加注释1111122222333334444555556666777788889999
 
     from django.core.management import execute_from_command_line
 
